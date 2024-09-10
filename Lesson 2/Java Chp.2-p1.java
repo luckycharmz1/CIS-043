39cf73c3376b477eca77959da3c0909e255b00eb
Charmaine Washington <cwashington2011@outlook.com>
2024-09-10 16:17:32 -0700

    Update Java Chp.2-p1.java

--- Lesson 2/Java Chp.2-p1.java
+++ Lesson 2/Java Chp.2-p1.java
@@ -1,5 +1,22 @@
-public class Welcome1 {
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chapter.exercises;
+
+/**
+ *
+ * @author cwash
+ */
+public class Chapter2Exercises {
+
     public static void main(String[] args) {
-        System.out.println("welcome to Java Programming!");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        
     }
 }
+
+
