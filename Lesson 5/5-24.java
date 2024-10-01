fac3053645ed3d3b01991259b8b10af613d4e640
Charmaine Washington <cwashington2011@outlook.com>
2024-09-30 17:11:30 -0700

    Create 5-24.java

---
+++ Lesson 5/5-24.java
@@ -0,0 +1,31 @@
+public class DiamondPrinting {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the upper part of the diamond
+
+        // Upper part of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print asterisks
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+
+        // Lower part of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print asterisks
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
